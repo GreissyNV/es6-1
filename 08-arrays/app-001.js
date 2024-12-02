fcc89c21ae34c4a277281b40fe9779cf59f41540
Greissy Naranjo <greissynv@gmail.com>
2024-12-02 14:49:27 +0100

    correcto

--- 08-arrays/app-001.js
+++ 08-arrays/app-001.js
@@ -3,7 +3,9 @@ const numberList = [12, 34, 56, 79, 98];
 const mix = [12, "hola", true, null];
 
 //Constructor 
-const frutas = new Array("Manzanas", "Pera", "Naranja");
+//const frutas = new Array("Manzanas", "Pera", "Naranja");
+let frutas = ["Manzana", "Pera", "Naranja"];
+console.log(typeof frutas);
 
 let x;
 //Obtener el valor de un elemento del array
@@ -12,5 +14,10 @@ x = numberList[1] + numberList[3];
 x = `Mi fruta preferida es ${frutas[2]}`;
 x = frutas.length;
 frutas[0] = "fresa";
+frutas[frutas.length] = ["piña", "melón"];
+frutas[frutas.length] = "cacahuetes";
 
-console.log(x);
+frutas.unshift("Sandia");
+
+console.log(frutas);
+console.log(typeof frutas);
