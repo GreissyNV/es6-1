8c9a6c5287f59c7085a0c61779314ac60fed6cce
Greissy Naranjo <greissynv@gmail.com>
2024-12-03 13:31:07 +0100

    cambios

--- 09-funciones/js/app-01.js
+++ 09-funciones/js/app-01.js
@@ -18,16 +18,36 @@ debugger
 
 // saludo("Mario", "Bros");
 
-const nombre = prompt("Escribe tu nombre");
-const apellido = prompt("Escribe tu apellido");
-saludo(nombre, apellido);
+// const nombre = prompt("Escribe tu nombre");
+// const apellido = prompt("Escribe tu apellido");
+// saludo(nombre, apellido);
 
-//hoisting
-function saludo(nombre, apellido) {
-  console.log(`Hola ${nombre} ${apellido}`);
+// //hoisting
+// function saludo(nombre, apellido) {
+//   console.log(`Hola ${nombre} ${apellido}`);
+// }
+
+
+//crea una funcion a la que se le pase un numero e imprima en la consola su tabla de multiplicar
+/**
+ * imprimirTabla(1);
+ * 1 x 1 = 1
+ * 1 x 2 = 2
+ * 1 x 3 = 3
+ */
+
+const a = parseFloat(prompt("Dame un numero: "));
+function multiplicar(num1, num2, num3, num4, num5, num6, num7, num8, num9, num10) {
+  console.log(num1 * [num1 * num10]);
 }
+let resultado;
+if (operador === "*") {
+  resultado = 1 * 2 * 3 * 4 * 5 * 6 * 7 * 8 * 10
+}
+//mostrar el resultado
+alert("El resultado es: " + resultado);
+
+multiplicar(1 * (1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
 
 
-//crea una funcion a la que se le pase un numero e imprima en la concola su tabla de multiplicar
-
-
+ 
