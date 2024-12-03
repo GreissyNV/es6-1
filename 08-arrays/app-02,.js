07ec37a6bd277cf6985d50182adeca2cc3f6ecd5
Greissy Naranjo <greissynv@gmail.com>
2024-12-03 10:36:37 +0100

    actualizado

--- 08-arrays/app-02,.js
+++ 08-arrays/app-02,.js
@@ -1,11 +1,16 @@
 //Metodos arrays
 let x;
 const arr = [28,34,56,73,890,55];
-//Añadir un elemento al final
-//arr[arr.length] = 34;
+// Añadir un elemento al final
+// arr[arr.length] = 34;
 arr.push(100, 20, 44);
-arr.pop(); //Elimina el ultimo elemento
+arr.pop(); // Elimina el ultimo elemento
 arr.unshift(2000) //Añade al principio
 arr.shift(); // Quita el primer elemento
-x = arr;
+//arr.reverse(); // Para invertir
+
+x = arr.includes(103);
+x = arr.indexOf(344); //si añadimos algo que no es verdad nos contestara -1 pero no debemos hacerlo
+//x = arr;
 console.log(x);
+console.log(arr);
