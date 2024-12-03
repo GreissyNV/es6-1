c8ebd437f7742813ef23d5f7227e6f6b484920cf
Greissy Naranjo <greissynv@gmail.com>
2024-12-03 12:50:11 +0100

    funciones

--- 09-funciones/js/app-01.js
+++ 09-funciones/js/app-01.js
@@ -15,14 +15,19 @@ sumar(3, 4);
 
 //crea una funcion que le pasemos un nombre como parametro y lance un alert diciendo hola saludo
 debugger
-function saludo(nombre, apellido) {
-  console.log(`Hola ${nombre} ${apellido}`);
-}
+
 // saludo("Mario", "Bros");
 
 const nombre = prompt("Escribe tu nombre");
 const apellido = prompt("Escribe tu apellido");
 saludo(nombre, apellido);
 
+//hoisting
+function saludo(nombre, apellido) {
+  console.log(`Hola ${nombre} ${apellido}`);
+}
+
+
+//crea una funcion a la que se le pase un numero e imprima en la concola su tabla de multiplicar
 
 
