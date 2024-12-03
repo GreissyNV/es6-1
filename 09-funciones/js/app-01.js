b8e83cc8cd47b722a39a3262ec292b42ee7d55db
Greissy Naranjo <greissynv@gmail.com>
2024-12-03 14:46:58 +0100

    tarea

--- 09-funciones/js/app-01.js
+++ 09-funciones/js/app-01.js
@@ -16,38 +16,14 @@ sumar(3, 4);
 //crea una funcion que le pasemos un nombre como parametro y lance un alert diciendo hola saludo
 debugger
 
-// saludo("Mario", "Bros");
+saludo("Mario", "Bros");
 
-// const nombre = prompt("Escribe tu nombre");
-// const apellido = prompt("Escribe tu apellido");
-// saludo(nombre, apellido);
+const nombre = prompt("Escribe tu nombre");
+const apellido = prompt("Escribe tu apellido");
+saludo(nombre, apellido);
 
-// //hoisting
-// function saludo(nombre, apellido) {
-//   console.log(`Hola ${nombre} ${apellido}`);
-// }
-
-
-//crea una funcion a la que se le pase un numero e imprima en la consola su tabla de multiplicar
-/**
- * imprimirTabla(1);
- * 1 x 1 = 1
- * 1 x 2 = 2
- * 1 x 3 = 3
- */
-
-const a = parseFloat(prompt("Dame un numero: "));
-function multiplicar(num1, num2, num3, num4, num5, num6, num7, num8, num9, num10) {
-  console.log(num1 * [num1 * num10]);
+//hoisting
+function saludo(nombre, apellido) {
+  console.log(`Hola ${nombre} ${apellido}`);
 }
-let resultado;
-if (operador === "*") {
-  resultado = 1 * 2 * 3 * 4 * 5 * 6 * 7 * 8 * 10
-}
-//mostrar el resultado
-alert("El resultado es: " + resultado);
 
-multiplicar(1 * (1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
-
-
- 
---
+++ 09-funciones/js/app-02.js
@@ -0,0 +1,30 @@
+//crea una funcion a la que se le pase un numero e imprima en la consola su tabla de multiplicar
+/**
+ * imprimirTabla(1);
+ * 1 x 1 = 1
+ * 1 x 2 = 2
+ * 1 x 3 = 3
+ */
+ 
+function imprimirTabla(num) {
+  for (let i = 1; i <=10 ; i++) {
+    // console.log(`1 x ${i} ${1 * i}`);
+  let res = 1 * i;
+  console.log("1 x " + i + " = " + res);
+  }
+ }
+
+// let numero = prompt("Escribe un número");
+// imprimirTabla(numero);
+
+function suma(a, b) {
+  //return a + b;
+  return res;
+}
+
+let resultado = suma(1,2);
+console.log(resultado);
+
+//escribe una funcion que pase de euros a dolares
+let cambio = multiplicacion(1 , 1.05);
+console.log(cambio);
