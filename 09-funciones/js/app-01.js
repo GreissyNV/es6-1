874450eca2867a2a31b135f3b27871ea5f5ef59f
Greissy Naranjo <greissynv@gmail.com>
2024-12-03 12:46:20 +0100

    parametros

--- 09-funciones/js/app-01.js
+++ 09-funciones/js/app-01.js
@@ -11,7 +11,18 @@ function suma(num1, num2) {
   console.log(num1 + num2);
 }
 
-suma(3, 4);
+sumar(3, 4);
 
 //crea una funcion que le pasemos un nombre como parametro y lance un alert diciendo hola saludo
-saludo("Ratatuille");-> "Hola Ratatuille"
+debugger
+function saludo(nombre, apellido) {
+  console.log(`Hola ${nombre} ${apellido}`);
+}
+// saludo("Mario", "Bros");
+
+const nombre = prompt("Escribe tu nombre");
+const apellido = prompt("Escribe tu apellido");
+saludo(nombre, apellido);
+
+
+
